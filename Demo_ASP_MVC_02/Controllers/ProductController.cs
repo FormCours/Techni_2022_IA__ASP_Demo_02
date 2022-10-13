e5f718b8affcb2256de563801bd70c1cbfc93e1d
studentdot0600 <studentdot0600@TFNSDOTDE0600A>
2022-10-13 15:43:32 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_ASP_MVC_02/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Demo_ASP_MVC_02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_ASP_MVC_02.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static IList<ProductViewModel> _Products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Exemple",
+                Description = "Element Hardcodé",
+                Price = 3.14
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_Products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductAddViewModel productAdd)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _Products.Add(new ProductViewModel()
+            {
+                Id = _Products.Select(p => p.Id).Max() + 1,
+                Name = productAdd.Name,
+                Description = productAdd.Description,
+                Price = (double)productAdd.Price
+            });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Demo_ASP_MVC_02/Models/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_ASP_MVC_02.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class ProductAddViewModel
+    {
+        [Required]
+        [MinLength(5)]
+        public string Name { get; set; }
+
+        [MaxLength(5000)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        //[RegularExpression("^[0-9]+(\\,[0-9]{1,2})?$")]
+        public double? Price { get; set; }
+    }
+}
